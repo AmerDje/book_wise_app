90d531ce922e0e8e5d3b62aa34ddb9629e88e31e
AmerDje <amer.dje10n20@gmail.com>
2023-09-26 23:03:58 +0100

    create custom app bar

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,27 @@
+import 'package:book_wise_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset(
+          Assets.logo,
+          height: 40,
+          width: 80,
+        ),
+        const Icon(
+          FontAwesomeIcons.magnifyingGlass,
+          size: 20,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:book_wise_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: [CustomAppBar()],
+      ),
+    );
+  }
+}
